46c088b29fc63f2570d0063b5c4785b319ed05e5
mchue <40476339+mchue@users.noreply.github.com>
2019-09-17 07:33:26 -0600

    Add files via upload

---
+++ Activity01.java
@@ -0,0 +1,23 @@
+public class Activity01{
+  public static void main(String[] args){
+  
+/** Name, Age, and Annual Income
+  * Write a program that declares the following:
+  * a String variable named name
+  * an int variable named age
+  * a double variable named annualPay
+  */
+    
+/** Store your age, name, and desired annual income as literals in these variables. 
+  * The program should display these values on the screen in a manner similar to the followings:
+  *  EX: My name is Joe Mahoney, my age is 26 and I hope to earn $100000.0 per year. 
+  */
+    
+    String name = "Joe Mahoney";
+    int age = 26;
+    double annualPay = 1000000.0;
+    
+    System.out.print("My name is "+name+", my age is "+age+" and I hope to earn $"+annualPay+" per year.");
+    
+  }
+}
