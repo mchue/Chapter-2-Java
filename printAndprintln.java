ee97990f4851c825554a5d62ca902505e8d9a14e
mchue <40476339+mchue@users.noreply.github.com>
2019-09-17 19:57:10 -0600

    Add files via upload

---
+++ printAndprintln.java
@@ -0,0 +1,31 @@
+public class printAndprintln{
+  public static void main(String[] args){
+ 
+  /** This example is using print
+    * output: HelloWorld!
+    */
+    System.out.print("Hello");
+    System.out.print("World");
+    System.out.print("!");
+    
+    System.out.println(); // printing to next line.
+    
+  /** This example is using println
+     * output:
+     * Hello
+     * World
+     * !
+     */    
+    System.out.println("Hello");
+    System.out.println("World");
+    System.out.println("!");
+    
+    
+  /** This example is useing both: print and println
+    * output: HelloWorld!
+    */
+    System.out.print("Hello");
+    System.out.print("World");
+    System.out.println("!");
+  }
+}
